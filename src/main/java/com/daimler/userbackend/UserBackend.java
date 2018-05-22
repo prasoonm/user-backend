89ecec306d84554d7b6b74c521c1746375b4d634
Prasoon <prasoon.mathummal@daimler.com>
2018-05-22 22:53:11 +0200

    Created Spring Boot application UserBackend

---
+++ src/main/java/com/daimler/userbackend/UserBackend.java
@@ -0,0 +1,11 @@
+package com.daimler.userbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBackend {
+    public static void main(String args[]) {
+        SpringApplication.run(UserBackend.class, args);
+    }
+}
