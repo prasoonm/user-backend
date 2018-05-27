237d1b91a0079424a4f8b49c4c9b941e70b50a37
Prasoon <prasoon.mathummal@daimler.com>
2018-05-27 15:08:38 +0200

    Enabled CORS Cross-Origin Resource Sharing

--- src/main/java/com/daimler/userbackend/UserController.java
+++ src/main/java/com/daimler/userbackend/UserController.java
@@ -1,5 +1,6 @@
 package com.daimler.userbackend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
